0a0045ad930abf82e68bab3a6db8e8f10e876c6b
Nathan Maynes <vicktor3@yahoo.com>
2014-03-04 21:58:45 -0500

    Updated the entire application.

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/node_test');
+
 var app = express();
 
 // all environments
@@ -20,6 +24,8 @@ app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -28,8 +34,15 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+//GETS
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.get('/helloworld', routes.helloworld);
+app.get('/userlist', routes.userlist(db));
+app.get('/newuser', routes.newuser);
+
+//POSTS
+app.post('/adduser', routes.adduser(db));
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,54 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
+
+exports.helloworld = function(req, res){
+	res.render('helloworld', { title: 'Hello World!', body: 'Example body text.' });
+};
+
+exports.userlist = function(db) {
+    return function(req, res) {
+        var collection = db.get('usercollection');
+        collection.find({},{},function(e,docs){
+            res.render('userlist', {
+                "userlist" : docs
+            });
+        });
+    };
+};
+
+exports.newuser = function(req, res){
+	res.render('newuser', {title: 'Add New User'});
+};
+
+exports.adduser = function(db) {
+    return function(req, res) {
+
+        // Get our form values. These rely on the "name" attributes
+        var userName = req.body.username;
+        var userEmail = req.body.useremail;
+        var userRole = req.body.userrole;
+
+        // Set our collection
+        var collection = db.get('usercollection');
+
+        // Submit to the DB
+        collection.insert({
+            "username" : userName,
+            "email" : userEmail,
+            "role" : userRole
+        }, function (err, doc) {
+            if (err) {
+                // If it failed, return error
+                res.send("There was a problem adding the information to the database.");
+            }
+            else {
+                // If it worked, set the header so the address bar doesn't still say /adduser
+                res.location("userlist");
+                // And forward to success page
+                res.redirect("userlist");
+            }
+        });
+
+    }
+}
