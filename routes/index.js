18ebb11f1526f9aae4d72fb4a567369b6f50b055
Nathan Maynes <vicktor3@yahoo.com>
2014-03-06 22:01:34 -0500

    Updated the page layouts
    Added containers to make the page content display uniformly.

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'UberWatcher', body: 'Keep an eye on your students' });
 };
 
 exports.helloworld = function(req, res){
